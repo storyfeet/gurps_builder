c0daec47839521eb37fd0b80d62c4f4dad661bc3
storyfeet <thebeaky@psmail.net>
2020-02-22 12:59:36 +0000

    now have a more home brew repl thing, it's no perfect, but it's almost useable

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,8 @@ import * as repl from "repl";
 import * as fs from "fs";
 import * as process from "process";
 import * as calc from "./calc";
+import * as path from "path";
+import * as inquirer from "inquirer"
 
 interface Costed{    
     cost:number;
@@ -21,10 +23,8 @@ class Data{
         this.skills = {}; 
         this.traits = {}; 
     }
-
 }
 
-let dt = new Data();
 
 
 class Player{
@@ -36,6 +36,8 @@ class Player{
     will:number;
     per:number;
     fp:number;
+    traits:[DataRef];
+    items:[DataRef];
     thrust_dmg():string{
         return calc.thrust_dmg(this.ST);
     };
@@ -54,16 +56,85 @@ class Player{
         this.per = st;
         this.fp = st;
     }
+}
 
+class DataCard {
+    tx:string;
+    select:string[];
+    cost:number;
+    level?:number;
+    tags:string[];
+}
+
+class DataRef{
+    name:string;
+    cost:number;
+    num:number;
+}
+
+function load_data(fpath:string):any{
+    let f_ref = (s:string)=>path.join(process.cwd(),s);
+    let d1 = fs.readFileSync(f_ref(fpath),'UTF-8');
+    return JSON.parse(d1);
 }
 
 
-repl.start("-->").context.c = {
-    args:process.argv,
-    dat:dt,
-    players:{},
-    npcs:{},
-    build:(n:number)=>new Player(n),
-    td:calc.thrust_dmg,
-    sd:calc.swing_dmg,
-};
+async function build_character(loc:any,name:string):Promise<Player>{
+    let r = await inquirer.prompt([
+        {name:"strength",type:"number",message:"What is your strength?"}
+    ]);
+    loc[name] = new Player(r.strength as number);
+    return loc[name];
+
+}
+
+//build_character()//.then((s)=>console.log(s));
+
+
+async function run():Promise<number>{ 
+    let dat = load_data("data/advantages.json");
+    let ss = "";
+    while(true){
+        let p = await inquirer.prompt([
+            {name:"job",type:"input",message:">>"}
+        ]);
+        let s = p.job as string;
+        if (s.slice(-1) == '\\') {
+            ss = ss+ s.slice(0,-1);
+        }else {
+            s = ss + s;
+            ss = "";
+
+            try {
+                let v = await eval(s);
+                console.log(v);
+            }catch(e) {
+                console.log(e);
+            }
+        }
+    }
+}
+
+run();
+    /*
+async function run(){
+    let dat = load_data("data/advantages.json");
+ 
+    await repl.start("-->").context.c = {
+        args:process.argv,
+        dat:dat,
+        players:{},
+        npcs:{},
+        b_char:build_character,
+        td:calc.thrust_dmg,
+        sd:calc.swing_dmg,
+    };
+    done = true;
+}
+
+run();
+
+    */
+
+
+
